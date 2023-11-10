baa8bee210d25eb4efcc6181a39c221cddf1b697
TheNewBadger <83123367+andywoodruff6@users.noreply.github.com>
2023-11-10 16:41:59 -0600

    Update Cargo.lock, Cargo.toml, main.rs, tauri.conf.json, App.vue, AddOne.vue, and styles.css

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,15 +1,21 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+fn main() {
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![greet, add_one])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
-fn main() {
-    tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+#[tauri::command]
+fn add_one(number: i32) -> i32 {
+    println!("Hello from Rust! {}", number);
+    return number + 1;
 }
---
+++ src/components/AddOne.vue
@@ -0,0 +1,31 @@
+<script>
+import { ref } from "vue";
+import { invoke } from "@tauri-apps/api/tauri";
+
+export default {
+  setup() {
+    const number = ref(0);
+
+    async function addOne() {
+      number.value = await invoke("add_one", { number: number.value });
+    }
+
+    return { number, addOne };
+  }
+}
+</script>
+
+<template>
+    <div class="AddOne">
+      <p>Current value: {{ number }}</p>
+      <button @click="addOne">Add 1</button>
+    </div>
+</template>
+
+<style scoped>
+.AddOne:border {
+  border: 1px solid red;
+}
+
+
+</style>
