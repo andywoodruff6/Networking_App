1bd3b1e66ad8ee14148d5d1ae8b16e5e1a3f0a80
TheNewBadger <83123367+andywoodruff6@users.noreply.github.com>
2023-11-11 11:42:22 -0600

    Add database functionality with SQLite #10

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,11 +1,41 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+use rusqlite::{Connection, Result};
 
-fn main() {
+fn main() -> Result<()> {
+
+    let conn = match Connection::open("connecti.db") {
+        Ok(connection) => connection,
+        Err(e) => {
+            println!("Error opening connection: {}", e);
+            return Err(e.into())
+        }
+    };
+
+    match conn.execute(
+        "CREATE TABLE IF NOT EXISTS person (
+            id INTEGER PRIMARY KEY,
+            first_name   TEXT NOT NULL,
+            last_name    TEXT NOT NULL,
+            email        TEXT NOT NULL,
+            phone_number TEXT NOT NULL
+        )",
+        [],
+    ) {
+        Ok(_) => println!("Table created successfully"),
+        Err(e) => {
+            println!("Error creating table: {}", e);
+            return Err(e.into())
+        }
+    };
+
+
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![greet, add_one])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
+
+    Ok(())
 }
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
